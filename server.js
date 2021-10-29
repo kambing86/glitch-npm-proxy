660bd88bffe6990b0091bceb104381dc38ef28fa
Glitch (hello-express) <none>
2021-10-29 02:57:19 +0000

    🏏🐛 Checkpoint ./server.js:20075382/1788

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
-const express = require('express');
-const proxy = require('http-proxy-middleware');
-const cors = require('cors');
+const express = require("express");
+const proxy = require("http-proxy-middleware");
+const cors = require("cors");
 const app = express();
 
 app.use(cors());
 app.use(
-  '/search/*',
-  proxy({ target: 'https://www.npmjs.com', changeOrigin: true })
+  "/search/*",
+  proxy({ target: "https://www.npmjs.com", changeOrigin: true })
 );
 app.use(
-  '*',
-  proxy({ target: 'https://registry.npmjs.org', changeOrigin: true })
+  "/registry/*",
+  proxy({
+    target: "https://registry.npmjs.org",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/registry": "/" // remove base path
+    }
+  })
+);
+app.use(
+  "*",
+  proxy({ target: "https://registry.npmjs.org", changeOrigin: true })
 );
 
-
 const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
